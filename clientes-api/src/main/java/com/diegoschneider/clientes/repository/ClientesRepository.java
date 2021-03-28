48e69f39e3feac60313819b08b0a5bc058e2bd55
Diego Schneider <desenvolvedorjava87@gmail.com>
2021-03-27 22:52:34 -0300

    config restcontrolleradvice e ajustes nas camadas

--- clientes-api/src/main/java/com/diegoschneider/clientes/repository/ClientesRepository.java
+++ clientes-api/src/main/java/com/diegoschneider/clientes/repository/ClientesRepository.java
@@ -4,4 +4,7 @@ import com.diegoschneider.clientes.model.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClientesRepository extends JpaRepository<Cliente, Integer> {
+
+
+
 }
--- clientes-api/src/main/java/com/diegoschneider/clientes/resource/ClienteResource.java
+++ clientes-api/src/main/java/com/diegoschneider/clientes/resource/ClienteResource.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -40,7 +41,7 @@ public class ClienteResource {
     }
 
     @PostMapping
-    public ResponseEntity<Cliente> create(@RequestBody Cliente cliente){
+    public ResponseEntity<Cliente> create(@Valid @RequestBody Cliente cliente){
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(clientesRepository
                 .save(clienteService.salvar(cliente)));
