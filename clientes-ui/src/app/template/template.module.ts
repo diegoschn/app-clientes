e7fcf64681aab318acd4850489900519a7360845
Diego Schneider <desenvolvedorjava87@gmail.com>
2021-03-28 22:42:04 -0300

    create footer component

--- clientes-ui/src/app/template/template.module.ts
+++ clientes-ui/src/app/template/template.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     NavbarComponent,
     SidebarComponent,
+    FooterComponent,
   ],
   imports: [
     CommonModule
@@ -14,6 +16,7 @@ import { SidebarComponent } from './sidebar/sidebar.component';
   exports : [
     NavbarComponent,
     SidebarComponent,
+    FooterComponent
   ]
 })
 export class TemplateModule { }
